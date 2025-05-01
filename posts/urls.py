9298f81d73203a4d50f4ad4daec2b4a0d378a532
Joao Silva <joaodoer3@gmail.com>
2025-05-01 16:27:29 -0300

    base api for posts implemented

---
+++ posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from posts import views
+
+router = DefaultRouter()
+router.register(r'posts', views.PostViewSet, basename='snippet')
+
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
